6bc260cbb136355a49411d64841714d7f0fc8240
MontyPayton <urspectrans@gmail.com>
2022-11-16 12:27:23 +0500

    home proverka

--- js/add.js
+++ js/add.js
@@ -4,6 +4,7 @@ $('.color-bl a').on('click', function () {
   
     $('#popup-min').addClass('active')
     $('#popup-max').removeClass('active')
+    $('.btn-max').css('background', 'white');
 })
 $('.colse-popup').on('click', function () {
     $('#popup-min').removeClass('active')
@@ -14,6 +15,7 @@ $('.color-red a').on('click', function () {
   
     $('#popup-max').addClass('active')
     $('#popup-min').removeClass('active')
+    $('.btn-min').css('background', 'white');
 })
 $('.colse-popup').on('click', function () {
     $('#popup-max').removeClass('active')
@@ -29,15 +31,15 @@ $('.btn-min').on('click', function () {
     $('#popup-min').addClass('active')
     $('#popup-max').removeClass('active')
 })
+//=====================================================
 
 
-
-
-// $(document).ready(function(){
+$('.searh-icon').on('click', function () {
   
-//     $(".fa-search").click(function(){
-//       $(".wrap, .input").toggleClass("active");
-//       $("input[type='text']").focus();
-//     });
+    $('.input-head').fadeToggle()
+  
     
-//   });
+
+ 
+})
+

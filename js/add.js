f28477f93c239d1e595b89eb8995d2e7e2fbb9bb
MontyPayton <urspectrans@gmail.com>
2022-11-16 15:19:47 +0500

    15-19

--- js/add.js
+++ js/add.js
@@ -1,45 +1,29 @@
 
 
-$('.color-bl a').on('click', function () {
-  
-    $('#popup-min').addClass('active')
-    $('#popup-max').removeClass('active')
-    $('.btn-max').css('background', 'white');
+$('.input').on('click', function () {
+     $('#popup-min').addClass('active')
+     $('#popup-max' ).removeClass('active')
 })
-$('.colse-popup').on('click', function () {
+    $( '.colse-popup').on('click', function () {
     $('#popup-min').removeClass('active')
-   
-})
-
-$('.color-red a').on('click', function () {
-  
+   })
+$('.registration').on('click', function () {
     $('#popup-max').addClass('active')
     $('#popup-min').removeClass('active')
+})
+$('.colse-popup').on('click', function () {
+    $('#popup-max').removeClass('active')
+})
+//    ===========================================
+$('.btn-max').on('click', function () {
+    $('.btn-max').css('background', ' #ECECEC');
     $('.btn-min').css('background', 'white');
 })
-$('.colse-popup').on('click', function () {
-    $('#popup-max').removeClass('active')
-//    ===========================================
-})
-$('.btn-max').on('click', function () {
-  
-    $('#popup-max').addClass('active')
-    $('#popup-min').removeClass('active')
-})
 $('.btn-min').on('click', function () {
-  
-    $('#popup-min').addClass('active')
-    $('#popup-max').removeClass('active')
+    $('.btn-max').css('background', 'white');
+    $('.btn-min').css('background', '#ECECEC');
 })
 //=====================================================
-
-
 $('.searh-icon').on('click', function () {
-  
     $('.input-head').fadeToggle()
-  
-    
-
- 
 })
-

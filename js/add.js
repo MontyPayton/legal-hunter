e609e19344686383f25120198c7fbfadb5f4b160
MontyPayton <urspectrans@gmail.com>
2022-11-16 10:29:33 +0500

    2popup

--- js/add.js
+++ js/add.js
@@ -2,9 +2,42 @@
 
 $('.color-bl a').on('click', function () {
   
-    $('.popup-iner').addClass('active')
+    $('#popup-min').addClass('active')
+    $('#popup-max').removeClass('active')
 })
 $('.colse-popup').on('click', function () {
-    $('.popup-iner').removeClass('active')
+    $('#popup-min').removeClass('active')
    
 })
+
+$('.color-red a').on('click', function () {
+  
+    $('#popup-max').addClass('active')
+    $('#popup-min').removeClass('active')
+})
+$('.colse-popup').on('click', function () {
+    $('#popup-max').removeClass('active')
+//    ===========================================
+})
+$('.btn-max').on('click', function () {
+  
+    $('#popup-max').addClass('active')
+    $('#popup-min').removeClass('active')
+})
+$('.btn-min').on('click', function () {
+  
+    $('#popup-min').addClass('active')
+    $('#popup-max').removeClass('active')
+})
+
+
+
+
+// $(document).ready(function(){
+  
+//     $(".fa-search").click(function(){
+//       $(".wrap, .input").toggleClass("active");
+//       $("input[type='text']").focus();
+//     });
+    
+//   });
